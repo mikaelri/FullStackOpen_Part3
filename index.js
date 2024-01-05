2cbd37c3de9fc6ed2a2691a1f387cd8fbfd02709
mikaelri <mikael.rintala@outlook.com>
2024-01-05 14:37:38 +0200

    return ex. 3.2

--- index.js
+++ index.js
@@ -33,9 +33,20 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>');
   });
 
-  app.get('/api/persons', (request, response) => {
-    response.json(persons);
-  });
+  app.get('/info', (request, response) => {
+
+    const now = new Date()
+
+    response.send
+        (
+        `<p> Phonebook has info for ${persons.length} people</p>
+        <p>${now}</p>`
+        )
+    });
+
+app.get('/api/persons', (request, response) => {
+response.json(persons);
+});
 
 const PORT = 3001
 app.listen(PORT)
