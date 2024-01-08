7cf861f862ad689aa213ce87458f4e7ba30e09ab
mikaelri <mikael.rintala@outlook.com>
2024-01-08 15:27:29 +0200

    return ex.3.8

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(morgan('tiny'))
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+
+morgan.token('content', (request) =>
+    request.method === 'POST'
+    ? JSON.stringify(request.body)
+    : null
+  )
 
 
 
