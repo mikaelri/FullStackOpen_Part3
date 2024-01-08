5be48c5e45cf26fe54b4dd54714ee76b19467128
mikaelri <mikael.rintala@outlook.com>
2024-01-08 12:41:20 +0200

    return ex.3.5

--- index.js
+++ index.js
@@ -62,11 +62,37 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
-  
+
   response.status(204).end()
 })
 
+const generateID = () => {
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(p => p.id))
+  : 0
+  return maxId +1
+}
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    console.log('Invalid request: name or number is missing');
+    return response.status(400).json({
+      error:'name or number is missing'
+    })
+  }
+
+  const person = {
+    id: generateID(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
 
 
 const PORT = 3001
