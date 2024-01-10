9ab2859ba1ed43ed42980a42d9937f8387000815
mikaelri <mikael.rintala@outlook.com>
2024-01-10 17:08:15 +0200

    put fly.io to work with mongodb

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
