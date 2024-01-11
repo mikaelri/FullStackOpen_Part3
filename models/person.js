fb1bf234a9f7a7bdd02ee80d4887e4403259efd0
mikaelri <mikael.rintala@outlook.com>
2024-01-11 14:01:26 +0200

    return ex. 3.20

--- models/person.js
+++ models/person.js
@@ -17,9 +17,20 @@ mongoose.connect(url)
     name: {
       type: String,
       minlength: 3,
-      required: true
+      required: [true, 'name is required']
     },
-    number: String,
+    
+    number: {
+      type: String,
+      minlength: 8,
+      validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d{6}/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: [true, 'phonenumber is required']
+    }
   })
 
 personSchema.set('toJSON', {
