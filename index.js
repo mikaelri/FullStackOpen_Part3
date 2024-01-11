cd7fda67292ee067ceae092ea005142150e3069f
mikaelri <mikael.rintala@outlook.com>
2024-01-11 11:28:32 +0200

    return ex. 3.17

--- index.js
+++ index.js
@@ -88,6 +88,21 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  .then(updatePerson => {
+    response.json(updatePerson)
+  })
+  .catch(error => next(error))
+})
+
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
